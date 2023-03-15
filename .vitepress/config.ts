1672a0516aac94111a775e2b14c7f373931957be
heappynd <heappynd@163.com>
2023-03-15 10:06:24 +0800

    feat: add collection

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Examples', link: '/markdown-examples' },
+      { text: 'Collection', link: '/collection' },
       { text: 'Gists', link: '/gists' },
     ],
 
